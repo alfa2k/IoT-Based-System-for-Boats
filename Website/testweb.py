3bccc9724e230f1d18df7f8407635bb21c2f520a
fateen ahmed <65744353+alfa2k@users.noreply.github.com>
2024-03-12 15:52:56 -0400

    Update testweb.py

--- Website/testweb.py
+++ Website/testweb.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/live_location')
+def live_location():
+    return render_template('live-location.html')
+
+@app.route('/live_camera')
+def live_camera():
+    return render_template('live-cameras.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
